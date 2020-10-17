a407589a4475a5a0db81bdc5cb5cd38c1d232908
Jose Morales <jmorales072095@gmail.com>
2020-10-16 18:55:00 -0700

    added folder and files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (name_of_burger) VALUES ("Cheese Burger");
+INSERT INTO burgers (name_of_burger) VALUES ("Double Cheese Burger");
+INSERT INTO burgers (name_of_burger) VALUES ("Bacon Cheese Burger");
+INSERT INTO burgers (name_of_burger) VALUES ("Impossible Burger");
+INSERT INTO burgers (name_of_burger) VALUES ("4X4 Burger");
---
+++ public/js/burgers.js
@@ -0,0 +1,58 @@
+$(function() {
+    $(".change-burger").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("devoured");
+
+        var newDevourState = {
+            devoured: newDevoured
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                console.log("changed devoured to", newDevoured);
+                // Reload the page to get the updated list
+                loburgerion.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#ca").val().trim(),
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                loburgerion.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function(event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function() {
+                console.log("deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
